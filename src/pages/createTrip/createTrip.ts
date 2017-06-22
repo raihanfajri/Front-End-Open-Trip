a569f12acbeb2048bd6b5413311e76e85c6c398d
M RAIHAN FAJRI <raihanfajri1206@gmail.com>
2017-06-22 09:48:05 +0700

    bikin trip

--- src/pages/createTrip/createTrip.ts
+++ src/pages/createTrip/createTrip.ts
@@ -14,17 +14,22 @@ export class createTripPage {
   public tanggal: string;
   public detail: string;
   public input: string;
+  public noInput = false;
   constructor(public http: Http, public navCtrl: NavController) {
 
   }
   kirim(){
-    var headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    this.input = JSON.stringify({tripname : this.tripname, idadmin : this.idadmin, asal : this.asal, tujuan: this.tujuan, tanggal : this.tanggal, detail: this.detail});
-    console.log(this.input);
-    this.http.post("http://localhost:3000/trip", this.input, {headers: headers}).subscribe(data => {
-          let v = data.json();
-      });
-    this.navCtrl.pop();
+    if(this.asal == undefined || this.tujuan == undefined || this.tanggal == undefined  ||this.detail == undefined|| !this.detail || !this.tujuan || !this.tanggal || !this.asal){
+      this.noInput = true;
+    }else{
+      var headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      this.input = JSON.stringify({tripname : this.tripname, idadmin : this.idadmin, asal : this.asal, tujuan: this.tujuan, tanggal : this.tanggal, detail: this.detail});
+      console.log(this.input);
+      this.http.post("http://localhost:3000/trip", this.input, {headers: headers}).subscribe(data => {
+            let v = data.json();
+        });
+      this.navCtrl.pop();
+    }
   }
 }
