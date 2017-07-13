7b3073852e0da531d9e60d6c20b630694f61da71
irkhan <irkhankenji@gmail.com>
2017-07-13 22:11:41 +0700

    lalala

--- src/pages/profile-page/profile-page.ts
+++ src/pages/profile-page/profile-page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, AlertController, App} from 'ionic-angular';
+import { createTripPage } from '../createTrip/createTrip';
+import { Http,Headers } from '@angular/http';
+import { DataService } from '../tabs/tabs';
+import { LoginPage} from '../login/login';
 
 /**
  * Generated class for the ProfilePage page.
@@ -7,20 +11,74 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * See http://ionicframework.com/docs/components/#navigation for more info
  * on Ionic pages and navigation.
  */
-@IonicPage()
 @Component({
   selector: 'page-profile-page',
   templateUrl: 'profile-page.html',
 })
 export class ProfilePage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  dataService: any = new DataService
+  val=0
+  trips: any = []
+  iduser = 0
+  username: string
+  fotoprofil: string
+  hozt: string
+  constructor(public app: App ,public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController,public loadingCtrl: LoadingController,public http: Http) {
+    var user = window.localStorage.getItem('user')
+    user = JSON.parse(user)
+    this.iduser = user[0]['idUser']
+    this.username = user[0]['Username']
+    this.fotoprofil = user[0]['foto_profil']
+    this.hozt = this.dataService.getHost()
+    this.show()
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
   }
-  editPost(){}
-  hapusPost(){}
+  show(){
+    let loading = this.loadingCtrl.create({
+      content: `<ion-spinner name="bubbles">Loading, Please wait..</ion-spinner>`
+    });
+    loading.present();
+    var headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      this.http.get(this.dataService.getHost()+"/trip/showtrip/?iduser="+this.iduser, {headers: headers}).subscribe(data => {
+         this.trips = data.json()
+         console.log(this.hozt)
+         loading.dismiss()
+      },
+        err => {
+          let alert = this.alertCtrl.create({
+            title: 'Connection Error!',
+            subTitle: 'Please Check Your Connection',
+            buttons: ['Dismiss']
+          });
+          loading.dismiss()
+          alert.present();
+        });
+  }  
+  editPost(id){
+    this.navCtrl.push(createTripPage,id)
+  }
+  hapusPost(){
+    var headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      this.http.get(this.dataService.getHost()+"/trip/deletetrip/?idtrip="+this.val, {headers: headers}).subscribe(data => {
+         console.log("terhapus!")
+      },
+        err => {
+          let alert = this.alertCtrl.create({
+            title: 'Connection Error!',
+            subTitle: 'Please Check Your Connection',
+            buttons: ['Dismiss']
+          });
+          alert.present();
+        });
+  }
+  logoutProses(){
+    window.localStorage.removeItem("user")
+    this.app.getRootNav().setRoot(LoginPage);
+  }
 
 }
